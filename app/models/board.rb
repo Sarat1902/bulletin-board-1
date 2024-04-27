b6abeb3876247105de67ffac99f079a44c0f6069
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-27 19:36:35 +0000

    commit

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,17 +8,19 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
+  validates(:name, { :presence => true , :uniqueness => true })
+
   def active_posts
     board_id = self.id
     #matching_posts = Post.where({:board_id => "#{board_id}" AND :expires_on > "#{Data.today}"})
-    matching_posts = Post.where("board_id = ? AND DATE(expires_on) > ?", board_id, Date.today)
+    matching_posts = Post.where("board_id = ? AND DATE(expires_on) > ?", board_id, Date.today).order({:expires_on => :asc})
     return matching_posts
   end
 
   def expired_posts
     board_id = self.id
     #matching_posts = Post.where({:board_id => "#{board_id}" AND :expires_on > "#{Data.today}"})
-    matching_posts = Post.where("board_id = ? AND DATE(expires_on) < ?", board_id, Date.today)
+    matching_posts = Post.where("board_id = ? AND DATE(expires_on) < ?", board_id, Date.today).order({:expires_on => :asc})
     return matching_posts
   end
 end
